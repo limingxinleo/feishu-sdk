76422745bbdfed34ae01b64ddbab2bcd6ae37445
李铭昕 <715557344@qq.com>
2022-01-04 09:22:20 +0800

    Deleted useless code

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -16,10 +16,6 @@ class ConfigProvider
     public function __invoke(): array
     {
         return [
-            'dependencies' => [
-            ],
-            'commands' => [
-            ],
             'annotations' => [
                 'scan' => [
                     'paths' => [
